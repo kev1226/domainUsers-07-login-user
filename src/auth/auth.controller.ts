cb29f0f7f9a360b371196e72ded93aba939ccb31
kev1226 <kevin_alt12@hotmail.com>
2025-06-25 01:21:28 -0500

    feat: initialize 07-login-users microservice listening on port 3007 with Docker & CI/CD

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(
+    @Body()
+    loginDto: LoginDto,
+  ) {
+    // Logic for user login
+    return this.authService.login(loginDto);
+  }
+}
---
+++ src/auth/kafka/kafka-client.config.ts
@@ -0,0 +1,13 @@
+import { ClientsModuleOptions, Transport } from '@nestjs/microservices';
+import { KafkaServices } from './kafka-constants';
+
+export const kafkaClientConfig: ClientsModuleOptions = [
+  {
+    name: KafkaServices.USER_LOGIN_SERVICE,
+    transport: Transport.KAFKA,
+    options: {
+      client: { clientId: 'auth-service-login', brokers: ['3.232.44.31:9092'] },
+      consumer: { groupId: 'auth-service-login-group' },
+    },
+  },
+];
